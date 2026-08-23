163bb006ce95abee52b91367f5322bd7049ea0d3
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-08-24 01:14:57 +0300

    refactor: move constants to one file

---
+++ constants.py
@@ -0,0 +1,13 @@
+MAX_SUB_SPLIT_SIZE = 500
+CHUNK_OVERLAP = 100
+CHUNK_SIZE = 500
+THRESHOLD = 100
+STOP_WORDS = {
+    "what", "is", "are", "of", "a", "an", "the",
+
+    "about", "above", "after", "again", "all", "am", "any", "at",
+    "be", "because", "been", "before", "by", "for", "from",
+
+    "how", "i", "my", "me", "we", "our", "you", "your", "this", "that",
+    "which", "who", "whom", "with", "whats"
+}
