92d5db782fa6f8b25b04b70737b95b00b7af700e
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-08-24 23:57:41 +0300

    refactor: normalise stopwords

--- tokenizer.py
+++ tokenizer.py
@@ -5,7 +5,8 @@ def tokenize_normalised_text(normalized_text: str) -> list[str]:
     if not normalized_text:
         return []
 
+    stopwords = {word.lower() for word in constants.STOP_WORDS}
     # tokenize and remove stopwords
-    tokenized_string = [word for word in normalized_text.split() if word not in constants.STOP_WORDS]
+    tokenized_string = [word for word in normalized_text.split() if word not in stopwords]
 
     return tokenized_string
